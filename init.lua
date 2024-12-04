a076cb3363f675c847f51cb6eb919e93654ef32c
qinghuan <1484237245@qq.com>
2024-12-05 02:11:09 +0800

    remove vscode plugins

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 require("config.lazy")
 
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono_Nerd_Font_Mono:h8"
+  vim.o.guifont = "JetBrainsMono_Nerd_Font_Mono:h7"
   --vim.g.neovide_transparency = 0.9
   vim.g.neovide_fullscreen = true
   vim.g.neovide_remember_window_size = true
